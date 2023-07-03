dd99fd09b5d8ec2b3603bc973fa4153ae42e20fd
V-Cabral <116751480+V-Cabral@users.noreply.github.com>
2023-07-03 14:33:44 -0300

    tirei o SERVER_NAME

--- config/config.py
+++ config/config.py
@@ -1,6 +1,5 @@
 class BaseConfig(object):
     DEBUG = False
-    SERVER_NAME = "localhost:5000"
     DB_USER = "ertksa9vxqbb9p0xp6dp"
     DB_PASS = "pscale_pw_gezlCU4Czx7ylRWTj971teefkKY0VM1T3XHf9Qo8J67"
 
